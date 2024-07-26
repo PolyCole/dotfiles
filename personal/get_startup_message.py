8c4806b68b259c8a397a65d2348ac45df68cdc8f
Cole Polyak <colepolyak@gmail.com>
2024-07-26 09:30:02 -0600

    🟤 Update: 2024-07-26 @ 09:30:02

--- personal/get_startup_message.py
+++ personal/get_startup_message.py
@@ -1,12 +1,20 @@
 import os 
 import random
+import socket
+import re
 
-functional_width = 40
+def is_work_laptop(hostname):
+    return bool(re.match(r"^MAC-[A-Z0-9]{10}$", hostname))
+
+if is_work_laptop(socket.gethostname()):
+    functional_width = 39
+else:
+    functional_width = 40
 
 def main():
     messages = get_messages()
     selected_message = messages[random.randint(0, len(messages) - 1)]
-    print_message(get_lines(selected_message))
+    print_message(get_lines(selected_message)) 
 
 def get_messages():
     file = open(f"{os.environ['HOME']}/repos/dotfiles/personal/messages.txt", "r")
