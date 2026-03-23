c04609c129a92728b3c2efe5663d0d1095bb6730
Cole Polyak <colepolyak@gmail.com>
2026-03-22 21:59:22 -0600

    Extract docker functions to modules/docker.zsh

---
+++ modules/docker.zsh
@@ -0,0 +1,13 @@
+# Commands:
+#   containers       List running containers (ID and image)
+#   murderdocker     Aggressively prune all Docker resources
+
+alias containers="docker ps --format \"table {{.ID}} \t{{.Image}}\""
+
+# Docker is being annoying, let's just burn it to the ground.
+murderdocker() {
+  docker volume prune
+  docker container prune --filter "until=24h"
+  docker image prune -a --filter "until=24h"
+  docker network prune
+}
