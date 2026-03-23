537e4cd9e1ddc2c60e7478feef33da21b743d94d
Cole Polyak <colepolyak@gmail.com>
2026-03-22 22:03:53 -0600

    Extract advent functions to modules/advent.zsh

---
+++ modules/advent.zsh
@@ -0,0 +1,9 @@
+# Commands:
+#   aoc [day]            Generate Advent of Code template for given day
+#   advent-of-code [day] Same as aoc
+
+alias aoc="advent-of-code "
+
+function advent-of-code() {
+  python $HOME/repos/AdventOfCode/template_aoc.py --day $1
+}
