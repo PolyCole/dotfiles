fe3547bdb353cef348dbb79ccee2b2d6c07b5dff
Cole Polyak <colepolyak@gmail.com>
2026-03-22 22:01:35 -0600

    Extract crypto aliases to modules/crypto.zsh

---
+++ modules/crypto.zsh
@@ -0,0 +1,8 @@
+# Commands:
+#   crypto         Show cryptocurrency overview (rate.sx)
+#   eth            Show Ethereum price
+#   btc            Show Bitcoin price
+
+alias crypto="curl rate.sx"
+alias eth="curl rate.sx/eth"
+alias btc="curl rate.sx/btc"
