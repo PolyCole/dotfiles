100b4193899c6bd571d512b2fa492a2fdd760734
Cole Polyak <colepolyak@gmail.com>
2026-03-25 17:38:35 -0600

    Extract aliases and cronsch from .personal_config into machines/personal/aliases.zsh

---
+++ machines/personal/aliases.zsh
@@ -0,0 +1,10 @@
+alias cosy="code /Users/cole/repos/Cosy"
+
+alias prep-dev="cd ~/repos/prep && go install . && command prep --dev"
+alias prep="cd ~/repos/prep && go install . && command prep"
+
+cronsch() {
+  print "\n***************************************"
+  print "🟢 Dotfiles -- M,T,W,R,F,S,S   @8:30 pm"
+  print "***************************************"
+}
